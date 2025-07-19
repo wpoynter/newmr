aec79592730dbbf06a5ad38d92bfe611934e4314
Will Poynter <poynter.william@gmail.com>
2025-07-19 11:43:04 +0100

    Add initial theme scaffolding

---
+++ generations/third/newmr-theme/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Theme functions and definitions
+ */
+
+function newmr_theme_setup() {
+    add_theme_support( 'wp-block-styles' );
+}
+add_action( 'after_setup_theme', 'newmr_theme_setup' );
